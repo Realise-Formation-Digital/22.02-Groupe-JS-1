87f6de22d17283d649ad2b5fd4da08b7978fe8d9
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-03-25 11:49:43 +0100

    feat: eventlisterner page marchand

---
+++ script-js/game.js
@@ -0,0 +1,61 @@
+// Btn pour acheter Defence... PAGE MARCHAND.
+const acheterDefBtn = document.getElementById("buyDef")
+
+
+acheterDefBtn.addEventListener("click",function(){
+    console.log("Btn Acheter Deffence")
+})
+
+
+// Btn pour acheter Attaque... PAGE MARCHAND.
+const acheterAtqBtn = document.getElementById("buyAtq")
+
+
+acheterAtqBtn.addEventListener("click",function(){
+    console.log("Btn Acheter Attaque")
+})
+
+
+// Btn acheter Arm1...PAGE MARCHAND.
+const acheterArm1Btn = document.getElementById("buyArm1")
+
+
+acheterArm1Btn.addEventListener("click",function(){
+    console.log("Btn Acheter Arm 1")
+})
+
+
+// Btn acheter Arm2...PAGE MARCHAND.
+const acheterArm2Btn = document.getElementById("buyArm2")
+
+
+acheterArm2Btn.addEventListener("click",function(){
+    console.log("Btn Acheter Arm 2 ")
+})
+
+
+// Btn acheter Arm3...PAGE MARCHAND.
+const acheterArm3Btn = document.getElementById("buyArm3")
+
+
+acheterArm3Btn.addEventListener("click",function(){
+    console.log("Btn Acheter Arm 3")
+})
+
+
+// Btn acheter Arm4...PAGE MARCHAND.
+const acheterArm4Btn = document.getElementById("buyArm4")
+
+
+acheterArm4Btn.addEventListener("click",function(){
+    console.log("Btn Acherter Arm 4")
+})
+
+
+// Btn Inventory...PAGE MARCHAND.
+const inventoryPageMarchandBtn = document.getElementById("inventaire-btn-pagemarchand")
+
+
+inventoryPageMarchandBtn.addEventListener("click",function(){
+    console.log("Btn Ouvrir Inventaire")
+})
